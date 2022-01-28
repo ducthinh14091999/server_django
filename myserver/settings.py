79a4e10b546b5fb3294e46e91fee3cc6c8751602
unknown <thinhhh9@gmail.com>
2022-01-28 07:47:09 +0700

    add url media

--- myserver/settings.py
+++ myserver/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
